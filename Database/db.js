71aec25a45533048f269ec24bdd300f7ca192599
Marcos Vitor <osads.orquestra@gmail.com>
2022-06-10 18:28:24 -0300

    Conexão efetuada com mongodb

--- Database/db.js
+++ Database/db.js
@@ -1,29 +1,33 @@
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 
-const DB_PASS= process.env.DB_PASS;
-const DB_HOST= process.env.DB_HOST;
-const DB_USER= process.env.DB_USER;
-const DB_NAME= process.env.DB_NAME;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_NAME = process.env.DB_NAME;
 
 class Connection {
-    constructor() {
-      this.mongodb();
-    }
-    mongodb() {
-      mongoose.connect(
-          `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority` 
-          , {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-      }).then(() => {
-        console.log("Conexão DB realizada com sucesso!");
-      }).catch((exception) => {
-        console.log("Erro de conexão com o banco." + exception);
-      });
-    }
+  constructor() {
+    this.mongodb();
   }
-  
-module.exports =  new Connection();
+  mongodb() {
+    mongoose
+      .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false,
+          useCreateIndex: true,
+        }
+      )
+      .then(() => {
+        console.log("Conexão DB realizada com sucesso!");
+      })
+      .catch((exception) => {
+        console.log("Erro de conexão com o banco. " + exception);
+      });
+  }
+}
+
+module.exports = new Connection();
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
-const { Router, json } = require("express"); 
-const bcrypt = require('bcrypt');
+const { Router, json } = require("express");
+const bcrypt = require("bcrypt");
 
-const {UserModel, AdminModel} = require("../models/usuarios.js");
+const { UserModel, AdminModel } = require("../models/usuarios.js");
 const authMidd = require("../middlewares/auth.js");
 
 const LoginController = require("../controllers/loginController");
@@ -11,12 +11,12 @@ const alunoController = require("../controllers/alunoController.js");
 const EventsController = require("../controllers/eventsController.js");
 const ContatoController = require("../controllers/contatoController.js");
 const PreFormsController = require("../controllers/preFormsController.js");
-const inventarioControllers = require("../controllers/inventarioController")
+const inventarioControllers = require("../controllers/inventarioController");
 
 const routes = new Router();
 
 // LOGIN - Aluno, Músico, Admin
-routes.post('/login', LoginController.login);
+routes.post("/login", LoginController.login);
 
 // Contato
 
@@ -26,7 +26,11 @@ routes.delete("/contato/:pid", authMidd(["Admin"]), ContatoController.delete);
 routes.post("/contato", ContatoController.create);
 
 // Eventos Calendário
-routes.get("/events", authMidd(["Admin", "Musico", "Aluno"]), EventsController.list);
+routes.get(
+  "/events",
+  authMidd(["Admin", "Musico", "Aluno"]),
+  EventsController.list
+);
 routes.post("/events", authMidd(["Admin"]), EventsController.create);
 routes.put("/events/:pid", authMidd(["Admin"]), EventsController.update);
 routes.delete("/events/:pid", authMidd(["Admin"]), EventsController.delete);
@@ -40,16 +44,36 @@ routes.delete("/forms/:id", authMidd(["Admin"]), PreFormsController.delete);
 
 // Inventário
 routes.get("/inventario", authMidd(["Admin"]), inventarioControllers.list);
-routes.get("/inventario/:id", authMidd(["Admin"]), inventarioControllers.listOne);
+routes.get(
+  "/inventario/:id",
+  authMidd(["Admin"]),
+  inventarioControllers.listOne
+);
 routes.post("/inventario", authMidd(["Admin"]), inventarioControllers.create);
-routes.put("/inventario/:id", authMidd(["Admin"]), inventarioControllers.update);
-routes.delete("/inventario/:id", authMidd(["Admin"]), inventarioControllers.delete);
+routes.put(
+  "/inventario/:id",
+  authMidd(["Admin"]),
+  inventarioControllers.update
+);
+routes.delete(
+  "/inventario/:id",
+  authMidd(["Admin"]),
+  inventarioControllers.delete
+);
 
 //Musicos
 routes.get("/musicos", authMidd(["Admin"]), musicoController.list);
-routes.get("/musicos/:pid", authMidd(["Admin", "Musico"]), musicoController.listOne);
+routes.get(
+  "/musicos/:pid",
+  authMidd(["Admin", "Musico"]),
+  musicoController.listOne
+);
 routes.post("/musicos", musicoController.create);
-routes.put("/musicos/:id", authMidd(["Admin", "Musico"]), musicoController.update);
+routes.put(
+  "/musicos/:id",
+  authMidd(["Admin", "Musico"]),
+  musicoController.update
+);
 routes.delete("/musicos/:id", authMidd(["Admin"]), musicoController.delete);
 
 //Alunos
@@ -60,45 +84,60 @@ routes.put("/alunos/:pid", authMidd(["Admin"]), alunoController.update);
 routes.delete("/alunos/:pid", authMidd(["Admin"]), alunoController.delete);
 
 // Profile
-routes.get("/profile/:id", authMidd(["Admin", "Musico", "Aluno"]), ProfileController.list);
-routes.put("/profile/:id", authMidd(["Admin", "Musico", "Aluno"]), ProfileController.update);
+routes.get(
+  "/profile/:id",
+  authMidd(["Admin", "Musico", "Aluno"]),
+  ProfileController.list
+);
+routes.put(
+  "/profile/:id",
+  authMidd(["Admin", "Musico", "Aluno"]),
+  ProfileController.update
+);
 
 // Pesquisa de usuários
 routes.get("/users", (req, res) => {
-    const {nome, sexo, email, _role} = req.query;
-    UserModel.find(JSON.parse(JSON.stringify({nome, sexo, email, _role}))).select("-senha").then(users =>{ res.json( users ) });
-})
+  const { nome, sexo, email, _role } = req.query;
+  UserModel.find(JSON.parse(JSON.stringify({ nome, sexo, email, _role })))
+    .select("-senha")
+    .then((users) => {
+      res.json(users);
+    });
+});
 
 routes.get("/admin", async (req, res) => {
-    AdminModel.create({
-        email: "admin@sistema.com",
-        senha: "$2a$08$RSSVqdQZF9BfEKk.55DX8eQi2DIRtlg/8UAqQEyV/7KrgP6W9T7lC" // 12345678
-    }).then((admin) => {
-        res.json(admin);
-    });
+  AdminModel.create({
+    email: "admin@sistema.com",
+    senha: "$2a$08$RSSVqdQZF9BfEKk.55DX8eQi2DIRtlg/8UAqQEyV/7KrgP6W9T7lC", // 12345678
+    nome: "Uriel",
+    dataNascimento: "24/01/1972",
+    telefone: "(61) 98352-8168",
+    sexo: "M",
+  }).then((admin) => {
+    res.json(admin);
+  });
 });
 
 routes.get("/", (req, res, next) => {
-    res.status(200).json({
-        status: "Sucess", 
-        msg: "Api Orquestra rodando!"
-    });
+  res.status(200).json({
+    status: "Sucess",
+    msg: "Api Orquestra rodando!",
+  });
 });
 
 routes.use((req, res, next) => {
-    res.status(404).json({
-        error: true,
-        msg: 'Not Found'
-    });
-})
-
-routes.use((error, req, res, next) => {
-    console.log(error)
-    return res.status(500).json({
-        errror: true,
-        message: "Internal Server Error"
-    });
+  res.status(404).json({
+    error: true,
+    msg: "Not Found",
+  });
 });
 
+routes.use((error, req, res, next) => {
+  console.log(error);
+  return res.status(500).json({
+    errror: true,
+    message: "Internal Server Error",
+  });
+});
 
 module.exports = routes;
