99b930179c1fd05b661dde8b44d9d7b9f53b94ad
Marcos Vitor <osads.orquestra@gmail.com>
2022-06-17 16:51:45 -0300

    Ajuste no inventarioController e inventarioModel

--- models/inventarioModel.js
+++ models/inventarioModel.js
@@ -1,37 +1,34 @@
 const mongoose = require("mongoose");
 
 const inventarioSchema = new mongoose.Schema(
-    {
-        numero: {
-            type: String,
-            required: [true, "Campo data deve ser preenchido"],
-        },
-        descricao: {
-          type: String,
-          required: [true, "Campo título deve ser preenchido"],
-        },
-        codigo: {
-          type: String,
-          required: [true, "Campo data deve ser preenchido"],
-        },
-        bem: {
-          type: String,
-          required: false
-        },
-        cautela: {
-          musico: {
-            type: String,
-            required: false,
-          },
-          data: {
-            type: String,
-            required: false,
-          }
-        }
+  {
+    codigo: {
+      type: String,
+      required: [true, "Campo código deve ser preenchido"],
     },
-    {
-        timestamps: true,
-    }
-)
+    descricao: {
+      type: String,
+      required: [true, "Campo descrição deve ser preenchido"],
+    },
+
+    bem: {
+      type: String,
+      required: [true, "Campo bem deve ser preenchido"],
+    },
+    cautela: {
+      musico: {
+        type: String,
+        required: false,
+      },
+      data: {
+        type: String,
+        required: false,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("inventario", inventarioSchema);
